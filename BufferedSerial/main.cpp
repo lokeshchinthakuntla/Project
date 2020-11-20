cb150e30c6f37e1010eb796451169911b0640493
Parth_Patel <54857091+parthpatel10@users.noreply.github.com>
2020-11-19 23:31:15 -0500

    Update main.cpp

--- BufferedSerial/main.cpp
+++ BufferedSerial/main.cpp
@@ -2,29 +2,29 @@
 #include "mbed.h"
 #include "TextLCD.h"
 #include "HCSR.h"
-#include "BufferedSerial.h" 
+//#include "BufferedSerial.h" 
 #include <string>
-
+DigitalOut myled(p25);
 HCSR04  usensor(p9,p10);//allocating the lpc pins for the ultrasonic sensor
 //rs, e, d4-d7 using lpc pins for interfacing with LCD16X2 to display required text on
 TextLCD lcd(p19, p20, p21, p22, p23, p24,TextLCD::LCD16x2);
-Serial pc(p28, p27);// serial monitoring
-Serial SIM800L(p13, p14);// pin delclearation
+//Serial pc(p28, p27);
+//Serial SIM800L(p13, p14);
 unsigned int dist; //Distance variable declaration
 int a ;
 int b ;
-long GSMBAUD = 9600;
-long PCBAUD  = 9600;
-
+//long GSMBAUD = 9600;
+//long PCBAUD  = 9600;
 
 int main()
-{
+{  
+    myled=0;
     while(1) {
         usensor.start();       //Turns on ultrasonic sensor
         wait_ms(500);          //pauses for 500ms
         dist=usensor.get_dist_cm();    //Measured distance is recorded from sensor
-        pc.baud(PCBAUD);
-        SIM800L.baud(GSMBAUD);
+        //pc.baud(PCBAUD);
+        //SIM800L.baud(GSMBAUD);
         lcd.cls();              //Clears LCD screen
 
 
@@ -37,7 +37,8 @@ int main()
             lcd.printf("Smart Bin");
             lcd.locate(0,1);
             lcd.printf("garbage  : %ld%%",b);
-
+            myled=0;
+           
         }
 
         if ( dist > 30 ) {
@@ -46,40 +47,14 @@ int main()
             lcd.printf("Smart Bin");
             lcd.locate(0,1);
             lcd.printf(" no data ");
+            myled=0;    
+                
+            }
+        if(dist<=8) 
+        {
+              myled=1;
         }
-        if(dist<8) { // distance is set 70%
-            SIM800L.printf("AT");
-            SIM800L.printf("\r\n");
-            wait(0.1f);
-
-            SIM800L.printf("AT+CMGF=1");//confg text mode
-            SIM800L.printf("\r\n");
-            wait(0.1f);
-
-            SIM800L.printf("AT+CMGS=\"+16475812594\"");// connected and send text to number
-            SIM800L.printf("\r\n");
-            wait(0.1f);
-
-            SIM800L.printf("4410, keele street , M2N 2Y5, BIN4-10 Garbage bin- 70%");// gsm sending text to number
-            SIM800L.printf("\r\n");
-            wait(0.1f);
-            wait_ms(500);
-        }
-
-        while(pc.readable()) {  // continous read the distance if less
-            SIM800L.putc(pc.getc());
-            SIM800L.printf("%c", SIM800L.putc(pc.getc()));
-            SIM800L.printf("\r\n");
-        }
-        wait(0.1f);
-        while(SIM800L.readable()) {
-            pc.putc(SIM800L.getc());
-            pc.printf("%c", pc.putc(SIM800L.getc()));
-        }
-        wait(0.1f);
-
-
-    }
+    
 }
-
+}
 
