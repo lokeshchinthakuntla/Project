41c675461f02b292aa8ba27fc1bbbd32c2f7f46c
Parth_Patel <54857091+parthpatel10@users.noreply.github.com>
2020-12-01 09:27:14 -0500

    Update ESP8266.h

--- ESP8266/ESP8266.h
+++ ESP8266/ESP8266.h
@@ -25,24 +25,14 @@ void SendCMD(char * s);
 void Reset(void);
 bool RcvReply(char * r, int to);
 void GetList(char * l);
-void Join(char * id, char * pwd);
 void GetIP(char * ip);
 void SetMode(char mode);
-void Quit(void);
-void SetSingle(void);
 void SetMultiple(void);
-void GetConnStatus(char * st);
-void StartServerMode(int port);
-void CloseServerMode(void);
-void setTransparent(void);
 void startTCPConn(char * IP, int port);
 void sendURL(char *URL, char *command);
 
 private:
 Serial comm;
-void AddEOL(char * s);
-void AddChar(char * s, char c);
-void itoa(int c, char s[]);
 
 };
   
